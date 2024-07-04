c55074793c1d31b3692bd2d4cbcff3575f7facce
Joaquim <oliver-junior@hotmail.com>
2024-07-04 19:47:51 -0300

    Create z_multiple_ux_widgets.py

---
+++ z_multiple_ux_widgets.py
@@ -0,0 +1,11 @@
+import kivy
+from kivy.app import App
+from kivy.core.window import Window
+
+Window.size = (1120,630)
+class ClassiqueApp(App):
+    def build(FloatLayout):
+        pass
+
+if __name__ == '__main__':
+    ClassiqueApp().run()
